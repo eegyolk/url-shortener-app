1deffa32bb8b34d3c9eb16c1ba468b289c54b081
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-09-13 11:22:11 +0800

    Temp design for this page

--- src/pages/DomainPage.vue
+++ src/pages/DomainPage.vue
@@ -1,11 +1,216 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page>
+    <div
+      class="row no-wrap justify-evenly items-center q-ma-lg bg-white"
+      style="height: 50px"
+    >
+      <span class="text-h6 q-pl-lg">Add Domain</span>
+
+      <q-space />
+
+      <q-btn
+        flat
+        class="q-pr-lg"
+        color="primary"
+        label="Add Domain"
+        :no-caps="true"
+        @click="persistent = true"
+      />
+    </div>
+
+    <div class="col q-ma-lg bg-white">
+      <q-table
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+        :separator="separator"
+      />
+    </div>
+
+    <div class="col q-ma-lg bg-white">
+      <q-dialog
+        v-model="persistent"
+        persistent
+        transition-show="scale"
+        transition-hide="scale"
+      >
+        <q-card class="bg-primary text-white" style="width: 500px">
+          <q-card-section>
+            <div class="text-h6">Add Domain</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none bg-white">
+            <q-input
+              outlined
+              class="q-pt-md"
+              type="text"
+              placeholder="Enter your domain"
+              :no-error-icon="true"
+            />
+          </q-card-section>
+
+          <q-card-actions align="right" class="bg-white text-primary">
+            <q-btn flat label="Save" />
+            <q-btn flat label="Cancel" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </div>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent, ref } from "vue";
 
+const columns = [
+  {
+    name: "name",
+    required: true,
+    label: "Dessert (100g serving)",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "calories",
+    align: "center",
+    label: "Calories",
+    field: "calories",
+    sortable: true,
+  },
+  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
+  { name: "carbs", label: "Carbs (g)", field: "carbs" },
+  { name: "protein", label: "Protein (g)", field: "protein" },
+  { name: "sodium", label: "Sodium (mg)", field: "sodium" },
+  {
+    name: "calcium",
+    label: "Calcium (%)",
+    field: "calcium",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "iron",
+    label: "Iron (%)",
+    field: "iron",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+];
+
+const rows = [
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: "14%",
+    iron: "1%",
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: "8%",
+    iron: "1%",
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: "6%",
+    iron: "7%",
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: "3%",
+    iron: "8%",
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: "7%",
+    iron: "16%",
+  },
+  {
+    name: "Jelly bean",
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: "0%",
+    iron: "0%",
+  },
+  {
+    name: "Lollipop",
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: "0%",
+    iron: "2%",
+  },
+  {
+    name: "Honeycomb",
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: "0%",
+    iron: "45%",
+  },
+  {
+    name: "Donut",
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: "2%",
+    iron: "22%",
+  },
+  {
+    name: "KitKat",
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: "12%",
+    iron: "6%",
+  },
+];
+
 export default defineComponent({
   name: "DomainPage",
+  setup() {
+    return {
+      separator: ref("vertical"),
+      persistent: ref(false),
+      columns,
+      rows,
+    };
+  },
 });
 </script>
